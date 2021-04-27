f03b04d45e0505513023efa9e9e54747764faa51
NguyenDuyPhuc01012001 <19522038@gm.uit.edu.vn>
2021-04-27 21:10:41 +0700

    no message

--- QuanLyNhaHang/QuanLyNhaHang/Login.xaml.cs
+++ QuanLyNhaHang/QuanLyNhaHang/Login.xaml.cs
@@ -12,7 +12,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
 namespace QuanLyNhaHang
 {
     /// <summary>
@@ -23,6 +22,60 @@ namespace QuanLyNhaHang
         public MainWindow()
         {
             InitializeComponent();
+            txbPassword.Visibility = Visibility.Collapsed;
+        }
+        private void DisablePasswordBox()
+        {
+            passwordBox.Visibility = Visibility.Collapsed;
+        }
+        private void DisablePasswordTextBox()
+        {
+            txbPassword.Visibility = Visibility.Collapsed;
+        }
+        private void EnablePasswordBox()
+        {
+            passwordBox.Visibility = Visibility.Visible;
+        }
+        private void EnablePasswordTextBox()
+        {
+            txbPassword.Visibility = Visibility.Visible;
+        }
+        private void UnmaskPassword()
+        {
+            txbPassword.Text = passwordBox.Password;
+        }
+        private void UpdatePassword()
+        {
+            passwordBox.Password = txbPassword.Text;
+        }
+        private void ShowPassword()
+        {
+            UnmaskPassword();
+            DisablePasswordBox();
+            EnablePasswordTextBox();
+            passwordShow.Kind = MaterialDesignThemes.Wpf.PackIconKind.Eye;
+        }
+        private void HidePassword()
+        {
+            UpdatePassword();
+            EnablePasswordBox();
+            DisablePasswordTextBox();
+            passwordShow.Kind = MaterialDesignThemes.Wpf.PackIconKind.EyeOff;
+        }
+        private void passwordShow_Click(object sender, RoutedEventArgs e)
+        {
+            if (passwordShow.Kind == MaterialDesignThemes.Wpf.PackIconKind.EyeOff)
+            {
+                ShowPassword();
+
+            }
+            else
+                HidePassword();
+        }
+
+        private void passwordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
