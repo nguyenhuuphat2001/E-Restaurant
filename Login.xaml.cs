d5feef22de9e524c54cfb8f2aa3e9ea0370ff480
thinhhja2001 <81420944+thinhhja2001@users.noreply.github.com>
2021-04-27 16:24:48 +0700

    Add files via upload
    Hide/show password

---
+++ Login.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+namespace QuanLyNhaHang
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            txbPassword.Visibility = Visibility.Collapsed;
+        }
+        private void DisablePasswordBox()
+        {
+            passwordBox.Visibility = Visibility.Collapsed;
+        }
+        private void DisablePasswordTextBox()
+        {
+            txbPassword.Visibility = Visibility.Collapsed;
+        }
+        private void EnablePasswordBox()
+        {
+            passwordBox.Visibility = Visibility.Visible;
+        }
+        private void EnablePasswordTextBox()
+        {
+            txbPassword.Visibility = Visibility.Visible;
+        }
+        private void UnmaskPassword()
+        {
+            txbPassword.Text = passwordBox.Password;
+        }
+        private void UpdatePassword()
+        {
+            passwordBox.Password = txbPassword.Text;
+        }
+        private void ShowPassword()
+        {
+            UnmaskPassword();
+            DisablePasswordBox();
+            EnablePasswordTextBox();
+            passwordShow.Kind = MaterialDesignThemes.Wpf.PackIconKind.Eye;
+        }
+        private void HidePassword()
+        {
+            UpdatePassword();
+            EnablePasswordBox();
+            DisablePasswordTextBox();
+            passwordShow.Kind = MaterialDesignThemes.Wpf.PackIconKind.EyeOff;
+        }
+        private void passwordShow_Click(object sender, RoutedEventArgs e)
+        {
+            if (passwordShow.Kind == MaterialDesignThemes.Wpf.PackIconKind.EyeOff)
+            {
+                ShowPassword();
+                
+            }
+            else
+                HidePassword();
+        }
+
+        private void passwordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
