dfc5c027658943f831edd134a3a951d261ebe4c7
xuanthai2 <68328065+xuanthai2@users.noreply.github.com>
2021-05-17 15:14:29 +0700

    thêm, sửa xoá meal
    + sửa, xoá (chưa form)

--- QuanLyNhaHang/QuanLyNhaHang/MainTemplate/AddNewMeal.xaml.cs
+++ QuanLyNhaHang/QuanLyNhaHang/MainTemplate/AddNewMeal.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using QuanLyNhaHang.DAO;
+using QuanLyNhaHang.DTO;
 
 namespace QuanLyNhaHang
 {
@@ -23,5 +25,109 @@ namespace QuanLyNhaHang
         {
             InitializeComponent();
         }
+
+
+
+        #region events
+        private void btnConfirm_Click(object sender, RoutedEventArgs e)
+        {
+            string name = txtNameMeal.Text;
+            int categoryID = 0;
+            string category = txtSupplierMeal.Text;
+
+            switch (category)
+            {
+                case "Hải sản":
+                    categoryID = 1;
+                    break;
+                case "Nông sản":
+                    categoryID = 2;
+                    break;
+                case "Lâm sản":
+                    categoryID = 3;
+                    break;
+                case "Nước":
+                    categoryID = 4;
+                    break;
+            }
+            float price = float.Parse(txtUnityPrice.Text);
+
+            if (FoodDAO.Instance.AddMeal(name, categoryID, price))
+            {
+                MessageBox.Show("Thành công");
+                if (addMeal != null)
+                    addMeal(this, new EventArgs());
+            }
+            else
+            {
+                MessageBox.Show("Không thành công");
+            }
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void EditMealEvent_Click(object sender, RoutedEventArgs e)
+        {
+            string name = txtNameMeal.Text;
+            int categoryID = 0;
+            string category = txtSupplierMeal.Text;
+            int id = Convert.ToInt32(txtIDMeal.Text);
+            float price = float.Parse(txtUnityPrice.Text);
+
+
+            if (FoodDAO.Instance.EditMeal(id, name, categoryID, price))
+            {
+                MessageBox.Show("Thành công");
+                if (editMeal != null)
+                    editMeal(this, new EventArgs());
+            }
+            else
+            {
+                MessageBox.Show("Không thành công");
+            }
+        }
+
+        private void DeleteMealEvent_Click(object sender, RoutedEventArgs e)
+        {
+            int id = Convert.ToInt32(txtIDMeal.Text);
+
+            if (FoodDAO.Instance.DeleteMeal(id))
+            {
+                MessageBox.Show("Thành công");
+                if (deleteMeal != null)
+                    deleteMeal(this, new EventArgs());
+            }
+            else
+            {
+                MessageBox.Show("Không thành công");
+            }
+        }
+
+        private event EventHandler addMeal;
+        public event EventHandler AddMeal
+        {
+            add { addMeal += value; }
+            remove { addMeal -= value; }
+        }
+
+        private event EventHandler editMeal;
+        public event EventHandler EditMeal
+        {
+            add { editMeal += value; }
+            remove { editMeal -= value; }
+        }
+
+        private event EventHandler deleteMeal;
+        public event EventHandler DeleteMeal
+        {
+            add { DeleteMeal += value; }
+            remove { DeleteMeal -= value; }
+        }
+        #endregion
+
+
     }
 }
